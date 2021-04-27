f1677cc8b6a4af09d5e735674a06d3ce9791b81e
KroshkoNikolay <xamjofoux@gmail.com>
2021-04-27 23:52:43 +0300

    add basic toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    MatToolbarModule,
     NoopAnimationsModule
   ],
   providers: [],
